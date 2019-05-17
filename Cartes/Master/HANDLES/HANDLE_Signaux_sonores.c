da43cad4d83fda3454591998ce2a08a42d008c82
Xavier Jannin <xav.jann1@gmail.com>
2019-05-17 17:49:09 +0200

    Ajout des signaux sonores

--- Cartes/Master/HANDLES/HANDLE_Signaux_sonores.c
+++ Cartes/Master/HANDLES/HANDLE_Signaux_sonores.c
@@ -19,15 +19,62 @@ char* HANDLE_Signaux_sonores(Commande* commande) {
 
   // ASS : Acquisition de signaux sonores
   if (strcmp("ASS", cmd) == 0) {
+		strcpy(response, "slave");
   }
 
   // SD : Génération de signaux sonores
   else if (strcmp("SD", cmd) == 0) {
+	int j;
+    int F = 6, P = 250, W = 500, B = 3;
+    for (j = 0; j < nbParams; j++) {
+      // F: code fréquence
+      if (params[j] == 'F') {
+        if (valeurs[j] >= 1 && valeurs[j] <= 99)
+          F = valeurs[j];
+        else
+          hasError = 1;
+      }
+
+      // P: Durée son
+      else if (params[j] == 'P') {
+        if (valeurs[j] >= 1 && valeurs[j] <= 99)
+          P = valeurs[j];
+        else
+          hasError = 1;
+      }
+
+      // W: Durée silence
+      else if (params[j] == 'W') {
+        if (valeurs[j] >= 0 && valeurs[j] <= 99)
+          W = valeurs[j];
+        else
+          hasError = 1;
+      }
+
+      // B: Nombre de bips
+      else if (params[j] == 'B') {
+        if (valeurs[j] >= 1 && valeurs[j] <= 99)
+          B = valeurs[j];
+        else
+          hasError = 1;
+      } 
+			
+			// Non reconnue:
+			else {
+				hasError = 1;
+			}
+    }
+    
+    // Si pas d'erreur, envoie la commande à la carte Slave:
+    if (!hasError) strcpy(response, "slave");
   }
 
   // Si aucune commande reconnue:
   else
     isCommand = 0;
+	
+	if (hasError) strcpy(response, "error");
+  else if(isCommand && strcmp(response, "rien") == 0) strcpy(response, "ok");
 
   return response;
 }
